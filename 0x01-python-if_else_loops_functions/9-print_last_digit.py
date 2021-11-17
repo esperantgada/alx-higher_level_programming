8a4ff0eff1e2bec022871a39e5d07dbd33f16c36
Esperant GADA <esperantgada.com>
2021-11-16 16:44:34 -0800

    Review style

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def print_last_digit(number):
     if number < 0:
         number = ((-1) * number) % 10
@@ -7,5 +8,4 @@ def print_last_digit(number):
         number = number % 10
 
     print("{:d}".format(number), end="")
-
     return number
