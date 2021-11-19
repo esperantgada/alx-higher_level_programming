54bc38431f89fa8113b4968330b15edf3dbdd09a
Esperant GADA <esperantgada.com>
2021-11-18 16:03:11 -0800

    Submit new solution

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    l = len(sys.argv)
+    if l == 1:
+        print('{} arguments.'.format(l - 1))
+    elif len(sys.argv) == 2:
+        print('{} argument:'.format(l - 1))
+    else:
+        print('{} arguments:'.format(l - 1))
+    for i in range(l):
+        if i == 0:
+            continue
+        print('{:d}: {:s}'.format(i, sys.argv[i]))
