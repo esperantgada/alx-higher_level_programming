9efb7b89fea157794231eabce7127cb895f5fa67
Esperant GADA <esperantgada.com>
2021-11-18 16:19:50 -0800

    Review code

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-
     import sys
-
-    l = len(sys.argv)
-    if l == 1:
-        print('{} arguments.'.format(l - 1))
-    elif len(sys.argv) == 2:
-        print('{} argument:'.format(l - 1))
+total = len(sys.argv)
+if total <= 1:
+    print("0 argument.")
+else:
+    if total == 2:
+        print("{:d} argument:".format(total - 1))
     else:
-        print('{} arguments:'.format(l - 1))
-    for i in range(l):
-        if i == 0:
-            continue
-        print('{:d}: {:s}'.format(i, sys.argv[i]))
+        print("{:d} argument:".format(total - 1))
+        for i in range(1, total):
+            print("{:d}: {}".format(i, sys.argv[1]))
