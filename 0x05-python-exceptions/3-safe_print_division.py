52c83410acacce0cc8b62b5867ca58f0ee29ceac
Esperant GADA <esperantgada.com>
2022-01-08 07:13:20 -0800

    Submit new solution

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def safe_print_division(a, b):
+    result = None
+    try:
+        result = a / b
+        print("Inside result: {}".format(result))
+    except:
+        print("Inside result: {}".format(result))
+    finally:
+        return result
